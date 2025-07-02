fae73cc0fe880ef622159cbe77219c4182b749ca
vaibhav <vaibhavgarg3210@gmail.com>
2025-07-02 23:04:38 +0530

    Add swagger dependency and find the correct version for springdoc -open-api

--- section-01-h2_database/accounts/src/main/java/com/learn/accounts/AccountsApplication.java
+++ section-01-h2_database/accounts/src/main/java/com/learn/accounts/AccountsApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API Documentation", version = "v1"))
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class AccountsApplication {
 
